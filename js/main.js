671e83daab5bed630d9f598419561c0ccb33917a
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-07-01 20:28:29 +0700

    Загрузка файлов

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function func(){  
+    var menu=document.querySelector(".menu");
+    var menuBtn=document.querySelector(".menu__button");
+    menu.classList.toggle('active');	
+    menuBtn.classList.toggle('active');
+}
